7016ce0d7599ef88e9046541d67b1e900a68581c
Toshio UESHBIA <t.ueshiba@aist.go.jp>
2022-01-10 04:05:19 +0900

    Add license descriptions

--- TU/algorithm.h
+++ TU/algorithm.h
@@ -1,11 +1,47 @@
 /*!
-  \mainpage	libTUTools++ - 配列，ベクトル，行列，画像等の基本的なデータ型とそれに付随したアルゴリズムを収めたライブラリ
+  \mainpage	libTUTools - 配列，ベクトル，行列，画像等の基本的なデータ型とそれに付随したアルゴリズムを収めたライブラリ
   \anchor	libTUTools
   \author	Toshio UESHIBA
-  \copyright	2002-2017年 植芝俊夫
+  \copyright	2002-2021年 植芝俊夫
+
+  \section copyright 著作権
+  Software License Agreement (BSD License)
+
+  Copyright (c) 2021, National Institute of Advanced Industrial Science and Technology (AIST)
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above
+     copyright notice, this list of conditions and the following
+     disclaimer in the documentation and/or other materials provided
+     with the distribution.
+   * Neither the name of National Institute of Advanced Industrial
+     Science and Technology (AIST) nor the names of its contributors
+     may be used to endorse or promote products derived from this software
+     without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+
+  Author: Toshio Ueshiba
 
   \section abstract 概要
-  libTUTools++は，配列，ベクトル，行列，画像等の基本的なデータ型とそれ
+  libTUToolsは，配列，ベクトル，行列，画像等の基本的なデータ型とそれ
   に付随したアルゴリズムを収めたライブラリである．現在実装されている主
   要なクラスおよび関数はおおまかに以下の分野に分類される．
 
